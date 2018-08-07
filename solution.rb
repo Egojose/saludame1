044cd953c50a5a9ee6b7e8cde2216d2312974315
Jose <josemicrobiologo@gmail.com>
2018-08-07 17:07:17 -0500

    saludame1

---
+++ solution.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+get '/' do
+    unless params[:nombre]
+        "Hola desconocido"
+    else
+        "Hola #{params[:nombre]}"
+    end
+end
+
+"/?nombre=Jose"
+params = {nombre: "Jose"}
